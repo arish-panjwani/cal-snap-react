79b765531d0e4a78ce5b5b0ec9c25806592a03fb
Ashish Lama <lama.ashish07@gmail.com>
2024-10-01 21:58:33 -0400

    Adds boostrap template for creating UI efficiently

---
+++ src/scenes/index.js
@@ -0,0 +1,33 @@
+import Navbar from "./layout/navbar";
+import SideBar from "./layout/sidebar";
+import Dashboard from "./dashboard";
+import Team from "./team";
+import Invoices from "./invoices";
+import Contacts from "./contacts";
+import Form from "./form";
+import Calendar from "./calendar";
+import Bar from "./bar";
+import Line from "./line";
+import Pie from "./pie";
+import Stream from "./stream";
+import FAQ from "./faq";
+import Geography from "./geography";
+import Login from "./login"
+
+export {
+  Navbar,
+  SideBar,
+  Dashboard,
+  Team,
+  Invoices,
+  Contacts,
+  Form,
+  Calendar,
+  Bar,
+  Line,
+  Pie,
+  Stream,
+  FAQ,
+  Geography,
+  Login
+};
