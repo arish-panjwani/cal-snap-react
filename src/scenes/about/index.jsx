94dee3fec1bde5b09b95beb5c2bdc6c373d9b4e2
Mueez Amjad <mueezurrehmanamja@loyalistcollege.com>
2024-12-11 13:21:24 -0500

    Images Updated, Description Added

--- src/scenes/about/index.jsx
+++ src/scenes/about/index.jsx
@@ -15,7 +15,7 @@ const AboutUs = () => {
     { "name": "Shabda Kafle", "role": "UI/UX", "image": "Shabda.jpeg" },
     { "name": "Utsav Harshadbhai Khamar", "role": "Data Analyst, Front End Developer", "image": "Utsav.jpeg" },
     { "name": "Moksh Naresh Jaiswal", "role": "Data Analyst, Data Science Member", "image": "Moksh.jpg" },
-    { "name": "Stephen David Chitilapalli Mathew", "role": "DevOps Lead, Data Analyst", "image": "Male.png" },
+    { "name": "Stephen David Chitilapalli Mathew", "role": "DevOps Lead, Data Analyst", "image": "Steve.JPG" },
     { "name": "Mukul Garg", "role": "Backend Lead", "image": "Mukul.jpg" },
     { "name": "Adarsh Shriram Pednekar", "role": "Backend Developer, Data Scientist", "image": "Adarsh.JPG" },
     { "name": "Safna Mohammed Fayas", "role": "Data Analyst", "image": "Safna.jpg" },
@@ -28,10 +28,11 @@ const AboutUs = () => {
   return (
     <div className="about-us">
       <div className="product-description">
-        <h1>About Our Product</h1>
+        <h1>About CalSnap</h1>
         <p>
-          Our product is designed to solve modern-day challenges with innovative solutions. 
-          Empowering users to achieve their goals effortlessly.
+        CalSnap empowers users to achieve their wellness goals with personalized insights and easy-to-use tools for tracking diet, 
+        physical activity, and health data. Combining advanced machine learning with an intuitive interface, 
+        it simplifies health management for a balanced, healthier lifestyle.
         </p>
       </div>
       <div className="team-members">
